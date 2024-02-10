fc7e719c05d89eb457ee0557713b5283349f3f22
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2024-02-11 03:32:11 +0530

    build changes

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
+import path from "path";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import { server ,app} from "./socket/socket.js";
 
+const __dirname = path.resolve();
 
 const port = process.env.PORT || 5000;
 
@@ -27,8 +28,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
 server.listen(port, () => {
