02d604511a4b1f1f78ccba3ca14620198f5df3af
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2024-02-11 11:19:17 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chat-app-ufu8.onrender.com",
+    origin: ["http://localhost:5000", "https://chat-app-ufu8.onrender.com"],
     credentials: true,
   })
 );
