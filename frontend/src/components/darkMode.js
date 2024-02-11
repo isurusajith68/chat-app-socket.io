7318bc63c17b991535cd4476e20a68182e0f0f65
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2024-02-11 23:57:33 +0530

    drak_mode button

--- frontend/src/components/darkMode.js
+++ frontend/src/components/darkMode.js
@@ -1,11 +1,11 @@
 import { useDarkModeContext } from "../context/DarkModeContext";
-import { BsFillMoonStarsFill } from "react-icons/bs";
+import { FaMoon } from "react-icons/fa6";
 import { IoMdSunny } from "react-icons/io";
 const DarkMode = () => {
   const { darkMode, setDarkMode } = useDarkModeContext();
 
   return (
-    <div className="fixed bottom-[100px] left-[94%] z-[999] h-[20px]  w-full max-md:left-[85%]">
+    <div className="fixed bottom-[105px]  left-[95%] z-[999] h-[20px]  w-full max-md:left-[82%]">
       <div
         className="borderRotation_dark_mode"
         onClick={() => {
@@ -19,9 +19,9 @@ const DarkMode = () => {
               style={{ color: "yellow", fontSize: "25px" }}
             />
           ) : (
-            <BsFillMoonStarsFill
-              className="test"
-              style={{ color: "darkorange", fontSize: "20px" }}
+            <FaMoon
+              className="test drop-shadow-xl"
+              style={{ color: "darkorange", fontSize: "25px" }}
             />
           )}
         </div>
